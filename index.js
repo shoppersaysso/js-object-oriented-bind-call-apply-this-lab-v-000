0f963ec565a9779eae00f652691948eaa90ce28b
Sara Hopper <sara.hopper@gmail.com>
2017-09-28 15:44:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ function invokeFunction(fn) {
   function justInvoke(fn){
 
   }
-}
+};
 
 function setThisWithCall(fn, thisValue) {
   let bob = thisValue;
-}
+};
 
 function setThisWithApply(fn, thisValue) {
-  
-}
+
+};
