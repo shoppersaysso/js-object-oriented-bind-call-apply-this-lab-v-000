9735c38dc52d5d140728479e3478221952f39b5f
Sara Hopper <sara.hopper@gmail.com>
2017-09-28 16:23:40 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,16 @@
+
+function justInvoke(fn) {
+  return fn(this.name);
+};
+
+function setThisWithCall(fn, thisValue) {
+  return fn.call(thisValue);
+};
+
+function setThisWithApply(fn, thisValue) {
+  return fn.apply(thisValue);
+};
+
+function returnNewFunctionOf(functionToBeCopied, thisValue) {
+  return functionToBeCopied.bind(thisValue);
+}
